ec053c8477bf48eacef75b8abb3e95636c16b0b8
Ezequiel <ezequiel.domenech@gmail.com>
2020-02-12 14:04:23 -0300

    agregados los metodos del modelo a desarrollar

---
+++ api/modules/v1/models/BookModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use \yii\db\ActiveRecord;
+
+class Books extends ActiveRecord{
+	
+	public function getBookByKey(){
+
+	}
+
+	public function setBook($book){
+
+	}
+
+	public function setActive($book){
+
+	}
+
+	public function setAccess($book){
+		
+	}
+
+}
---
+++ api/modules/v1/models/ClientModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use \yii\db\ActiveRecord;
+
+class Clients extends ActiveRecord{
+	
+	public function setClient($client){
+
+	}
+}
